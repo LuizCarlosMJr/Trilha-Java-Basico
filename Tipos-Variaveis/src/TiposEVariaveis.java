594f6bbd3ef37724280e57de92932e171203b3e4
LuizCarlosMJr <luiz.carlos.mjr2403@gmail.com>
2023-01-20 13:24:24 -0300

    Modificação de App para TiposEVariaveis

--- Tipos-Variaveis/src/TiposEVariaveis.java
+++ Tipos-Variaveis/src/TiposEVariaveis.java
@@ -1,5 +1,28 @@
-public class App {
+public class TiposEVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       /* byte idade =123;
+        short ano = 2022;
+        int cep = 1481000;//Se começar com Zero, tem que usar o tipo String
+        long cpf = 00000000031L; //Se começar com Zero, tem que usar o tipo String - Precisa terminar com L
+        float pi = 3.14F;
+        double salario = 1234.56;   
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; //Adaptação casting
+     */
+        // Cosntantes x variáveis
+        //int numeroNormal = 5;
+         //numeroNormal = 10
+        // System.out.println(numeroNormal);
+
+        //String
+        String primeiroNome = "Luiz Carlos";
+        String segundoNome = "M. júnor";
+
+        System.out.println(primeiroNome.concat(" ").concat(segundoNome)); //outraforma de concatenação
+    
     }
+
+
 }
