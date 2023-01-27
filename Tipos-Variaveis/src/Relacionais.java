905db8a55fcd556fd527082d6772052a157c8ebb
LuizCarlosMJr <luiz.carlos.mjr2403@gmail.com>
2023-01-26 21:55:13 -0300

    inclusao de Anatomia

--- Tipos-Variaveis/src/Relacionais.java
+++ Tipos-Variaveis/src/Relacionais.java
@@ -4,6 +4,21 @@ public class Relacionais {
         int numero1 = 3;
         int numero2 = 3;
 
+        String nome1 = "Luiz";
+        String nome2 = "Luiz";
+        String nome3 = new String("Luiz");
+
+        if(nome1 == nome2){
+            System.out.println("Os nomes são iguais");
+        }
+        if(nome1 == nome3){
+            System.out.println("Os nomes são iguais");
+        } else{
+            System.out.println("Os nomes são diferentes");
+        }
+
+       
+
        /*  //Exemplo 1
         boolean simNao = numero1 > numero2;
         System.out.println("A condição  numero1 > numero2 é " + simNao);
@@ -21,7 +36,7 @@ public class Relacionais {
         System.out.println("A condição  numero1 != numero2 é " + simNao);
        */
 
-          if(numero1 > numero2){
+        /*  if(numero1 > numero2){
             System.out.println("Numero 1 maior que numero 2");
                 }else if(numero1 < numero2){
                     System.out.println("Numero 1 menor que numero 2");
@@ -30,7 +45,7 @@ public class Relacionais {
                                  }else if(numero1 <= numero2){
                                      System.out.println("Numero 1 menor ou igual que numero 2");
                                         }  if (numero1 != numero2){
-                                            System.out.println("Numero 1 é diferente de numero 2");}
+                                            System.out.println("Numero 1 é diferente de numero 2");}*/ 
         
     }
 }
