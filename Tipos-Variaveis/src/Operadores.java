bfa13357e08239418dcc7b3d730f8d4840c743b9
LuizCarlosMJr <luiz.carlos.mjr2403@gmail.com>
2023-01-26 21:52:19 -0300

    Inclusao de OperadoresLogicos e Relacionais

---
+++ Tipos-Variaveis/src/Operadores.java
@@ -0,0 +1,48 @@
+public class Operadores {
+    public static void main(String[] args) {
+        // classe Operadores.java
+        double soma = 10.5 + 15.7;
+        // System.out.println(soma);//26.2
+
+        int subtração = 113 - 25;
+        // System.out.println(subtração);//88
+
+        int multiplicacao = 20 * 7;
+        // System.out.println(multiplicacao);//5
+
+        int divisao = 15 / 3;
+        // System.out.println(divisao);//5
+
+        int modulo = 18 % 3;
+        // System.out.println(modulo);//0
+
+        double resultado = (10 * 7) + (20 / 4);
+        // System.out.println(resultado);//75
+
+        // classe Operadores.java
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+        // System.out.println(nomecompelto);
+
+        // qual o resultado das expressoes abaixo? Usar o modo DEBUG (mostrará linha por
+        // linha - Seleciona a linha, Clica no Debug, F5(teclado) - começar F10(teclado) - para ir linha a linha
+        String concatenacao = "?";
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + 1 + 1 + "1";
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + 1;
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + "1";
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + 1 + 1 + 1;
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + (1 + 1 + 1);
+        System.out.println(concatenacao);
+
+    }
+
+}
---
+++ Tipos-Variaveis/src/OperadoresUnarios.java
@@ -0,0 +1,25 @@
+public class OperadoresUnarios {
+    public static void main(String[] args){
+        
+        int numero = 5;
+       // numero = - numero;
+       // System.out.println(numero);
+
+        //Incrementos|Decrementos - Repetição
+
+        //numero = numero + 1; é a mesma coisa que numero++;
+        //System.out.println(numero++); //dessa forma imprime 5
+       // System.out.println(numero);//imprimirá 6
+
+       // System.out.println(++numero);//primeiro soma depois mostra ao resultado 7
+
+        //booleanos
+
+        boolean variavel = true;
+        System.out.println(variavel);
+
+        variavel = !variavel;// forma de inveter o valor de um booleano
+        System.out.println(variavel);
+
+    }   
+}
